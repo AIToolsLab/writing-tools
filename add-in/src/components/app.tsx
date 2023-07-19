dd75ab50b5c728bc97eba04857fed85616dfe142
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2023-07-19 14:49:31 -0400

    A few new prompts

--- add-in/src/components/app.tsx
+++ add-in/src/components/app.tsx
@@ -16,6 +16,10 @@ export interface Card {
 
 const presetPrompts = [
     {
+        key: "Summary: thesis",
+        text: "What seems to be the thesis of this paragraph? Answer in one concise sentence."
+    },
+    {
         key: 'Summary: phrases',
         text: 'Step 1: List 10 important concepts in this paragraph, in the format 1. Concept: [concept as a complete sentence] Relevance: [relevance score, 10 best]. Step 3: Say FINAL OUTPUT. Step 4: Make a - list with the 3 concepts with highest relevance, in short phrases of 2 or 3 words.',
     },
@@ -31,6 +35,10 @@ const presetPrompts = [
     {
         key: 'Reactions: questions',
         text: 'As a reader, ask the writer 2 or 3 questions about definitions, logical connections, or some needed background information.',
+    },
+    {
+        key: "Advice",
+        text: "What advice would you give the writer to improve this paragraph? Respond in a bulleted list."
     },
     {
         key: 'Metaphors',
