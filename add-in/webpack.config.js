0790628a3f8863b74e4dca50e1e06588b17d65eb
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2023-07-21 13:39:55 -0400

    Add an extra slash to dev URL so the replacement works cleanly

--- add-in/webpack.config.js
+++ add-in/webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-const urlDev = 'http://localhost:3000';
+const urlDev = 'http://localhost:3000/';
 const urlProd = 'https://tools.kenarnold.org/';
 
 async function getHttpsOptions() {
