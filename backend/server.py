16eda300e11e8c03396aca5a21d451688323b35e
Souad Yakubu <121411073+souadyakubu@users.noreply.github.com>
2023-07-23 20:28:46 +0000

    initial

--- backend/server.py
+++ backend/server.py
@@ -1,34 +1,29 @@
 import json
 import os
-import sys
 import sqlite3
-from typing import List, Dict
-
-from pathlib import Path
+from typing import List
 
 import openai
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 from nlp import (
+    get_completion_reflections,
     gen_reflections_chat,
-    send_message,
-    ReflectionResponseInternal
+    fix_json_chat,
 )
-
-openai.organization = "org-9bUDqwqHW2Peg4u47Psf9uUo"
 
 # Read env file
 with open(".env", "r") as f:
     for line in f:
         key, value = line.split("=")
-        if key == "OPENAI_API_KEY":
-            openai.api_key = value.strip()
-        elif key == "OPENAI_ORGANIZATION":
-            openai.organization = value.strip()
+        os.environ[key] = value.strip()
+
+openai.organization = "org-9bUDqwqHW2Peg4u47Psf9uUo"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 class ReflectionRequestPayload(BaseModel):
     paragraph: str
@@ -36,13 +31,19 @@ class ReflectionRequestPayload(BaseModel):
 
 
 class ReflectionResponseItem(BaseModel):
-    reflection: str
+    text_in_HTML_format: str
+    sentence_number_in_paragraph: int
+    quality: float
 
-class ChatRequestPayload(BaseModel):
-    messages: List[Dict[str, str]]
 
 class ReflectionResponses(BaseModel):
     reflections: List[ReflectionResponseItem]
+
+
+class FeedbackPayload(BaseModel):
+    paragraph: str
+    prompt: str
+    feedback_type: str  # "Upvote" or "Reject"
 
 
 app = FastAPI()
@@ -66,11 +67,12 @@ with sqlite3.connect(db_file) as conn:
     c.execute(
         "CREATE TABLE IF NOT EXISTS requests (timestamp, prompt, paragraph, response, success)"
     )
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS feedback_logs (timestamp, prompt, paragraph, feedback_type)"
+    )
 
 
-async def get_reflections_chat(
-    request: ReflectionRequestPayload,
-) -> ReflectionResponses:
+async def get_reflections_chat(request: ReflectionRequestPayload) -> ReflectionResponses:
     # Check if this request has been made before
     with sqlite3.connect(db_file) as conn:
         c = conn.cursor()
@@ -84,41 +86,64 @@ async def get_reflections_chat(
             response_json = result[0]
             response = json.loads(response_json)
             # assume that the database stores only valid responses in the correct schema.
-            reflections_internal = ReflectionResponseInternal(**response)
+            # We check this below.
+            return ReflectionResponses(**response)
 
-        else:
-
-            # Else, make the request and cache the response
-            reflections_internal = await gen_reflections_chat(
-                writing=request.paragraph,
-                prompt=request.prompt
-            )
-
-            # Cache the response
-            # Use SQL timestamp
-            c.execute(
-                'INSERT INTO requests VALUES (datetime("now"), ?, ?, ?, ?)',
-                (request.prompt, request.paragraph, json.dumps(reflections_internal.dict()), "true"),
-            )
-
-    return ReflectionResponses(
-        reflections=[
-            ReflectionResponseItem(reflection=reflection)
-            for reflection in reflections_internal.reflections
-        ]
+    # Else, make the request and cache the response
+    response = await gen_reflections_chat(
+        writing=request.paragraph,
+        prompt=request.prompt
     )
+
+    # Attempt to parse JSON
+    try:
+        response_json = json.loads(response)
+        reflection_items = ReflectionResponses(**response_json)
+    except Exception as e1:
+        new_response = await fix_json_chat(response)
+
+        # Try to parse again
+        try:
+            response_json = json.loads(new_response)
+            reflection_items = ReflectionResponses(**response_json)
+        except Exception as e2:
+            # If it still doesn't work, log the error and fail out
+            with sqlite3.connect(db_file) as conn:
+                c = conn.cursor()
+                # Use SQL timestamp
+                c.execute(
+                    'INSERT INTO requests VALUES (datetime("now"), ?, ?, ?, ?)',
+                    (request.prompt, request.paragraph, json.dumps(dict(
+                        error=str(e2),
+                        response=response
+                    )), "false"),
+                )
+
+            raise e2
+
+    # Cache the response
+    with sqlite3.connect(db_file) as conn:
+        c = conn.cursor()
+        # Use SQL timestamp
+        c.execute(
+            'INSERT INTO requests VALUES (datetime("now"), ?, ?, ?, ?)',
+            (request.prompt, request.paragraph, json.dumps(
+                reflection_items.dict()), "true"),
+        )
+
+    return reflection_items
 
 
 @app.post("/reflections")
 async def reflections(payload: ReflectionRequestPayload):
-    return await get_reflections_chat(payload)
+    api = "chat"
 
+    if api == "chat":
+        return await get_reflections_chat(payload)
+    elif api == "completions":
+        # TODO: Update completion method
+        return get_completion_reflections(writing=payload.paragraph, prompt=payload.prompt)
 
-@app.post("/chat")
-async def chat(payload: ChatRequestPayload):
-    return await send_message(
-        messages=payload.messages
-    )
 
 @app.get("/logs")
 async def logs():
@@ -129,9 +154,19 @@ async def logs():
 
     return result
 
-# Get access to files on the server. Only for a production build.
-static_path = Path('../add-in/dist')
-app.mount("/static", StaticFiles(directory=static_path), name="static")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=int(sys.argv[1] if len(sys.argv) > 1 else 8000))
+@app.post("/log_feedback")
+async def log_feedback(payload: FeedbackPayload):
+    with sqlite3.connect(db_file) as conn:
+        c = conn.cursor()
+        # Use SQL timestamp
+        c.execute(
+            'INSERT INTO feedback_logs VALUES (datetime("now"), ?, ?, ?)',
+            (payload.prompt, payload.paragraph, payload.feedback_type),
+        )
+
+    return {"message": "Feedback logged successfully"}
+
+
+uvicorn.run(app, port=8000)
+
