9149fc3c3bc2c6fad4ea013e227f3669b539f8b0
Ray Cai Flanagan <hjkareus@gmail.com>
2023-07-30 14:30:01 -0400

    Adjust chat styles

--- add-in/src/components/chatMessage/index.tsx
+++ add-in/src/components/chatMessage/index.tsx
@@ -18,34 +18,46 @@ export default function ChatMessage(
 ) {
     return (
         <div className={classes.container}>
+            {
+                props.role !== 'assistant' ? (
+                    <div className={ classes.toolbar }>
+                        <FiRefreshCcw
+                            className={classes.icon}
+                            onClick={() => props.refresh(props.index)}
+                        />
+                    </div>
+                ) : (
+                    <div className={ classes.toolbar }>
+                        <FiTrash2
+                            className={classes.icon}
+                            onClick={() => props.deleteMessage(props.index)}
+                        />
+
+                        <TfiCommentAlt
+                            className={classes.icon}
+                            onClick={() => props.convertToComment(props.index)}
+                        />
+                    </div>
+                )
+            }
+            
             <div
-                className={`${classes.cardContainer}
-                    ${props.role === 'assistant' ? classes.aiMessage : ''}`}
+                className={`${classes.cardContainer}`}
             >
+                <div className={ classes.pfpContainer }>
+                    {
+                        props.role === 'user' ? (
+                            <img
+                                src="https://source.boringavatars.com/marble/30/Maria%20user"
+                                alt="User"
+                            />
+                        ) : (
+                            <div className={ classes.pfp } />
+                        )
+                    }
+                </div>
+
                 {props.content}
-
-                {
-                    props.role !== 'assistant' ? (
-                        <div className={ classes.toolbar }>
-                            <FiRefreshCcw
-                                className={classes.icon}
-                                onClick={() => props.refresh(props.index)}
-                            />
-                        </div>
-                    ) : (
-                        <div className={ classes.toolbar }>
-                            <FiTrash2
-                                className={classes.icon}
-                                onClick={() => props.deleteMessage(props.index)}
-                            />
-
-                            <TfiCommentAlt
-                                className={classes.icon}
-                                onClick={() => props.convertToComment(props.index)}
-                            />
-                        </div>
-                    )
-                }
             </div>
         </div>
     );
