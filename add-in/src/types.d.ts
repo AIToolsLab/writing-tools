70a01e536042f33b81e9c3fb50053dd79081038c
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2023-08-21 16:20:35 -0700

    Move the interfaces to existing types decl

--- add-in/src/types.d.ts
+++ add-in/src/types.d.ts
@@ -1,1 +1,14 @@
 declare module '*.css';
+
+interface ReflectionResponseItem {
+    reflection: string;
+}
+
+interface ReflectionResponses {
+    reflections: ReflectionResponseItem[];
+}
+
+interface CardData {
+    paragraphIndex: number;
+    body: string;
+}
