5c37f41f04a492200ed7620d6b940a4746610921
Matt Van Harn <mattvanharn@gmail.com>
2023-10-26 14:51:09 -0400

    Merge pull request from Matt Update landing page so addin can be added to app store

---
+++ add-in/src/landing-page/script.js
@@ -0,0 +1,7 @@
+document.getElementById('download-button').addEventListener('click', function() {
+    window.location.href = '/path-to-your-add-in';
+});
+
+document.getElementById('contact-button').addEventListener('click', function() {
+    window.location.href = '/path-to-your-contact-form';
+});
