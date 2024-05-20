c4baa0b82512b2755a585d91070b8e8d0a375b7a
Cobblestone5517 <kjy11gold@gmail.com>
2024-05-20 13:13:45 -0400

    Add searchBox component

---
+++ add-in/src/components/searchBox/index.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useRef, useState } from 'react';
+import { AiOutlineSearch, AiFillCloseCircle } from "react-icons/ai";
+
+import classes from './styles.module.css';
+
+// Handle auto resizing of the textarea
+function handleAutoResize(textarea: HTMLTextAreaElement): void {
+    textarea.style.height = '100%';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+}
+
+// TO DO: Modify/Revise the prompts
+const promptList = [
+    'What is the main point of this paragraph?',
+    'What are the important concepts in this paragraph? List three of them in dashes -',
+    'What are the claims or arguments presented in this paragraph? List three of them in dashes -',
+    'What are some potential counterarguments to the claims presented in this paragraph? Make tentative statements, list three of them in dashes -',
+    'What further evidence or examples would you like to see to support the claims presented in this paragraph? List three of them in dashes -',
+    'What outside the box questions do you have about this paragraph? List three of them in dashes -',
+    'What questions do you have about this paragraph as a writer? List three of them in dashes -',
+    'What questions do you have about this paragraph as a reader? List three of them in dashes -',
+];
+
+interface SearchBoxProps {
+	currentPrompt: string;
+	updatePrompt: (prompt: string) => void;
+}
+
+export const defaultPrompt = promptList[0];
+
+export function SearchBox(
+    props: SearchBoxProps
+): JSX.Element {
+    const { currentPrompt, updatePrompt } = props;
+
+    // Filter the prompt list based on the current prompt
+    const filteredPromptList = promptList.filter((prompt) =>
+        prompt.toLowerCase().includes(currentPrompt.toLowerCase())
+    );
+
+    // TO DO: handle empty prompt (maybe ask user to select a prompt from the list or hide the reflectionCard?)
+
+    return (
+        <div className={classes.searchBoxWrapper}>
+            <div className={classes.searchBox}>
+                <AiOutlineSearch className={classes.searchBoxIcon} />
+                <textarea
+                    defaultValue=""
+                    value={currentPrompt}
+                    placeholder="Search for a prompt..."
+                    onChange={(event) => updatePrompt(event.target.value)}
+                    className={classes.searchBoxInput}
+                    ref={ref => ref && handleAutoResize(ref)}
+                />
+                <AiFillCloseCircle
+                    className={classes.searchBoxClear}
+                    onClick={() => updatePrompt('')}
+                />
+            </div>
+            <div className={classes.searchBoxDropdown}>
+                <ul>
+                    {filteredPromptList.map((prompt, index) => (
+                        <li
+                            className={classes.searchBoxDropdownItem}
+                            key={index}
+                            onClick={() => updatePrompt(prompt)}
+                        >
+                            {prompt}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
