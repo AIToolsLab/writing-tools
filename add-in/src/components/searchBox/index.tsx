7682237a239729e31d807888837a142fa6471938
Geesing <chewgeesing@gmail.com>
2024-05-28 15:50:39 -0400

    Show up to 3 past prompt searches in completion recommendations

--- add-in/src/components/searchBox/index.tsx
+++ add-in/src/components/searchBox/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { AiOutlineFileSearch, AiFillCloseCircle } from 'react-icons/ai';
+import { AiOutlineFileSearch, AiFillCloseCircle, AiOutlineHistory } from 'react-icons/ai';
 import { FcNext } from 'react-icons/fc';
 
 import classes from './styles.module.css';
@@ -20,12 +20,18 @@ export function SearchBox(
 ): JSX.Element {
     const { updatePrompt, suggestedPrompts } = props;
 
+    const [prevPrompts, _updatePrevPrompts] = useState<string[]>([]);
     const [searchBoxText, updateSearchBoxText] = useState('');
     const [searchBoxTextSent, updateSearchBoxTextSent] = useState(false);
     const [dropdownVisible, updateDropdownVisible] = useState(false);
 
     // Filter the prompt list based on the current prompt
-    const filteredPromptList = suggestedPrompts.filter((prompt) =>
+    const filteredNewPromptList = suggestedPrompts.filter((prompt) =>
+        prompt.toLowerCase().includes(searchBoxText.toLowerCase())
+    );
+
+    // Filter the prompt list based on the current prompt
+    const filteredHistoryList = prevPrompts.filter((prompt) =>
         prompt.toLowerCase().includes(searchBoxText.toLowerCase())
     );
 
@@ -62,6 +68,10 @@ export function SearchBox(
                             event.preventDefault();
                             updatePrompt(searchBoxText);
                             updateSearchBoxTextSent(true);
+                            prevPrompts.unshift(searchBoxText);
+                            if (prevPrompts.length > 25) {
+                                prevPrompts.pop();
+                            }
                         }
                     } }
                     onFocus={ () => {
@@ -90,7 +100,7 @@ export function SearchBox(
                 <>
                     <hr/>
                     <ul>
-                        { filteredPromptList.map((prompt: string, index: number) => {
+                        { filteredHistoryList.slice(0, 3).map((prompt: string, index: number) => {
                             return (
                                 <li
                                     className={ classes.searchBoxDropdownItem }
@@ -102,7 +112,27 @@ export function SearchBox(
                                     } }
                                 >
                                     <div className={ classes.searchBoxDropdownIconWrapper }>
-                                        <FcNext className={ classes.searchBoxDropdownIcon } />
+                                        <AiOutlineHistory className={ classes.dropdownHistoryIcon } />
+                                    </div>
+                                    { prompt }
+                                </li>
+                            );
+                        }) }
+                    </ul>
+                    <ul>
+                        { filteredNewPromptList.map((prompt: string, index: number) => {
+                            return (
+                                <li
+                                    className={ classes.searchBoxDropdownItem }
+                                    key={ index }
+                                    onClick={ () => {
+                                        updatePrompt(prompt);
+                                        updateSearchBoxText(prompt);
+                                        updateSearchBoxTextSent(true);
+                                    } }
+                                >
+                                    <div className={ classes.searchBoxDropdownIconWrapper }>
+                                        <FcNext className={ classes.dropdownBulletIcon } />
                                     </div>
                                     { prompt }
                                 </li>
