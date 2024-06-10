b52f5bae8b2fd5e7aa095d649f25adc7524bad0d
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-06-10 14:26:56 -0400

    move chat hook to a separate file

--- add-in/src/pages/chat/index.tsx
+++ add-in/src/pages/chat/index.tsx
@@ -1,7 +1,6 @@
 import { useState, useContext } from 'react';
 
 import { AiOutlineSend } from 'react-icons/ai';
-import { fetchEventSource } from '@microsoft/fetch-event-source';
 
 import ChatMessage from '@/components/chatMessage';
 
@@ -10,71 +9,10 @@ import { UserContext } from '@/contexts/userContext';
 
 import { SERVER_URL } from '@/api';
 
+import { useChat } from '@/hooks/useChat';
+
 import classes from './styles.module.css';
 
-function useChat({ SERVER_URL, chatMessages, updateChatMessages, username}: 
-	{ SERVER_URL: string, chatMessages: any, updateChatMessages: any, username: string}
-) {
-	async function append(message: string) {
-
-		if (!message) return;
-
-		let newMessages = [
-			...chatMessages,
-			{ role: 'user', content: message },
-			{ role: 'assistant', content: '' }
-		];
-
-		updateChatMessages(newMessages);
-
-		await fetchEventSource(`${SERVER_URL}/chat`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				messages: [...chatMessages, { role: 'user', content: message }],
-				username: username
-			}),
-			onmessage(msg) {
-				const message = JSON.parse(msg.data);
-				const choice = message.choices[0];
-				if (choice.finish_reason === 'stop') return;
-				const newContent = choice.delta.content;
-				// need to make a new "newMessages" object to force React to update :(
-				newMessages = newMessages.slice();
-				newMessages[newMessages.length - 1].content += newContent;
-				updateChatMessages(newMessages);
-			}
-		});
-	}
-
-	async function regenMessage(index: number) {
-		// Resubmit the conversation up until the last message,
-		// so it regenerates the last assistant message.
-		const response = await fetch(`${SERVER_URL}/chat`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				messages: chatMessages.slice(0, index)
-			})
-		});
-
-		const responseJson = await response.json();
-
-		const newMessages = [...chatMessages];
-		newMessages[index + 1] = {
-			role: 'assistant',
-			content: responseJson
-		};
-
-		updateChatMessages(newMessages);
-	}
-
-	return { append, regenMessage };
-}
 
 export default function Chat() {
 	const { chatMessages, updateChatMessages } = useContext(ChatContext);
