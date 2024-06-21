d81e92c4845de7fc3729444c1c5b7007a026e000
Cobblestone5517 <kjy11gold@gmail.com>
2024-06-21 14:35:09 -0400

    Fix port

--- backend/get_env.py
+++ backend/get_env.py
@@ -17,8 +17,8 @@ DATABASE_URI_SECRET_NAME = "TestTextfocalsDBURI"
 
 # Set DEBUG_STATUS to "False" if running on production machine, "True" otherwise
 DEBUG_STATUS = "False" if socket.getfqdn() == "ds1.cs.calvin.edu" else "True"
-# Prod runs on 19570, dev runs on 3000
-PORT = "19570" if DEBUG_STATUS == "False" else "3000"
+# Prod runs on 19570, dev runs on 8000
+PORT = "19570" if DEBUG_STATUS == "False" else "8000"
 
 
 def get_keyvault(vault_name: str) -> SecretClient:
