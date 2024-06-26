9221da2a11d7e41a9bfdc29b43388bc248f9eefc
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-06-26 13:48:32 -0400

    gunicorn for deployment

---
+++ backend/gunicorn.conf.py
@@ -0,0 +1,10 @@
+bind = "127.0.0.1:19570"
+worker_class = "uvicorn.workers.UvicornWorker"
+wsgi_app = "server:app"
+# Need single worker because each worker would load an LLM
+workers = 1
+errorlog = "error.log"
+accesslog = "access.log"
+capture_output = True
+proc_name = "writing-tools"
+pidfile = "gunicorn.pid"
