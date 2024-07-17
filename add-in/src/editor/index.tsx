8c082a3bf231ffbe9df058624a8c4fc69ebd0c55
Cobblestone5517 <kjy11gold@gmail.com>
2024-07-17 10:41:11 -0400

    Use similar font to the Word Addin

--- add-in/src/editor/index.tsx
+++ add-in/src/editor/index.tsx
@@ -48,7 +48,7 @@ function App() {
 
 	return (
 		<div className={ classes.container }>
-            <div>
+            <div className={ classes.editor }>
                 <LexicalEditor
                     updateTextBeforeCursor={ docUpdated }
                 />
