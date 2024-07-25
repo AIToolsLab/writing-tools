e57549cc57a6eff25069fd88d709fede53b16324
Ray Cai Flanagan <hjkareus@gmail.com>
2024-07-25 10:54:33 -0400

    clean up imports

--- add-in/src/editor/index.tsx
+++ add-in/src/editor/index.tsx
@@ -1,18 +1,16 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { useRef } from 'react';
+import ReactDOM from 'react-dom';
 
 import classes from './styles.module.css';
 import QvE from '@/pages/qve';
 import LexicalEditor from './editor';
 
 function App() {
-	// const editorRef = React.useRef(null);
-
     // needs to be a ref so that we can use docRef.current in the editorAPI
-    const docRef = React.useRef('');
+    const docRef = useRef('');
 
 	// since this is a list, a useState would have worked as well
-	const selectionChangeHandlers = React.useRef<(() => void)[]>([]);
+	const selectionChangeHandlers = useRef<(() => void)[]>([]);
 
 	const handleSelectionChange = () => {
 		selectionChangeHandlers.current.forEach(handler => handler());
