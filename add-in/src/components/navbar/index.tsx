45edfc9404bd9542ff5f799d257dc63927c96ac2
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-09-27 11:26:16 -0400

    [MERGE] Restore navbar component from textfocals

---
+++ add-in/src/components/navbar/index.tsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+
+import { PageContext } from '@/contexts/pageContext';
+
+import classes from './styles.module.css';
+
+export default function Navbar() {
+	const { page, changePage } = useContext(PageContext);
+
+	return (
+		<nav className={ classes.nav }>
+			<p
+				onClick={ () => changePage('reflections') }
+				className={ page === 'reflections' ? classes.active : '' }
+			>
+				Reflections
+			</p>
+			<p
+				onClick={ () => changePage('focals') }
+				className={ page === 'focals' ? classes.active : '' }
+			>
+				Focals
+			</p>
+            
+			<p
+				onClick={ () => changePage('chat') }
+				className={ page === 'chat' ? classes.active : '' }
+			>
+				Chat
+			</p>
+		</nav>
+	);
+}
