da0a090c3da0c6214a89800ea93f23ddab624823
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-09-28 15:20:04 -0400

    refactor cleanup navbar

--- add-in/src/components/navbar/index.tsx
+++ add-in/src/components/navbar/index.tsx
@@ -4,30 +4,26 @@ import { PageContext } from '@/contexts/pageContext';
 
 import classes from './styles.module.css';
 
+const pageNames = [
+	{ name: 'reflections', title: 'Reflections' },
+	{ name: 'focals', title: 'Focals' },
+	{ name: 'chat', title: 'Chat' }
+];
+
 export default function Navbar() {
 	const { page, changePage } = useContext(PageContext);
 
 	return (
 		<nav className={ classes.nav }>
-			<p
-				onClick={ () => changePage('reflections') }
-				className={ page === 'reflections' ? classes.active : '' }
-			>
-				Reflections
-			</p>
-			<p
-				onClick={ () => changePage('focals') }
-				className={ page === 'focals' ? classes.active : '' }
-			>
-				Focals
-			</p>
-            
-			<p
-				onClick={ () => changePage('chat') }
-				className={ page === 'chat' ? classes.active : '' }
-			>
-				Chat
-			</p>
+			{ pageNames.map(({ name: pageName, title: pageTitle }) => (
+				<p
+					key={ pageName }
+					onClick={ () => changePage(pageName) }
+					className={ page === pageName ? classes.active : '' }
+				>
+					{ pageTitle }
+				</p>
+			)) }
 		</nav>
 	);
 }
