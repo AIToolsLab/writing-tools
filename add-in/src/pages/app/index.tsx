818d6a1f0a62079ab0935d1be739309002ac5b1f
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-09-28 15:29:49 -0400

    [MERGE] merge back unified app page from unified

--- add-in/src/pages/app/index.tsx
+++ add-in/src/pages/app/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 
 import { PageContext } from '@/contexts/pageContext';
+import { UserContext } from '@/contexts/userContext';
+
 
 import Layout from '@/components/layout';
 
@@ -8,6 +10,8 @@ import Home from '../home';
 import Focals from '../focals';
 import Chat from '../chat';
 import Login from '../login';
+import QvE from '../qve';
+import { wordEditorAPI } from '@/api/wordEditorAPI';
 
 export interface HomeProps {
 	isOfficeInitialized: boolean;
@@ -21,9 +25,19 @@ export default function App({ isOfficeInitialized }: HomeProps) {
 			</section>
 		);
 
+	const { username } = useContext(UserContext);
+
+	if (username.length === 0) return <Login />;
+
 	const { page } = useContext(PageContext);
 
-	if (page === 'login') return <Login />;
+	function getComponent(pageName: string) {
+		if (pageName === 'reflections') return <Home />;
+		if (pageName === 'focals') return <Focals />;
+		if (pageName === 'chat') return <Chat />;
+		if (pageName === 'qve') return <QvE editorAPI={ wordEditorAPI } />;
+		console.error('Invalid page name', pageName);
+	}
 
-	return <Layout>{ page === 'reflections' ? <Home /> : page === 'focals' ? <Focals/> : <Chat /> }</Layout>;
+	return <Layout>{ getComponent(page) }</Layout>;
 }
