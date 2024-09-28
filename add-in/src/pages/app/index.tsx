ac7ce8de37388f32bd1ea5eed192f645a6be3eee
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-09-28 15:17:25 -0400

    merge app router

--- add-in/src/pages/app/index.tsx
+++ add-in/src/pages/app/index.tsx
@@ -1,7 +1,14 @@
 import { useContext } from 'react';
 
+import { PageContext } from '@/contexts/pageContext';
 import { UserContext } from '@/contexts/userContext';
 
+
+import Layout from '@/components/layout';
+
+import Home from '../home';
+import Focals from '../focals';
+import Chat from '../chat';
 import Login from '../login';
 import QvE from '../qve';
 import { wordEditorAPI } from '@/api/wordEditorAPI';
@@ -13,7 +20,7 @@ export interface HomeProps {
 export default function App({ isOfficeInitialized }: HomeProps) {
 	if (!isOfficeInitialized)
 		return (
-			<section>
+			<section className="ms-welcome__progress ms-u-fadeIn500">
 				<p>Please sideload your addin to see app body.</p>
 			</section>
 		);
@@ -22,5 +29,15 @@ export default function App({ isOfficeInitialized }: HomeProps) {
 
 	if (username.length === 0) return <Login />;
 
-	return <QvE editorAPI={ wordEditorAPI }/>;
+	const { page } = useContext(PageContext);
+
+	function getComponent(pageName: string) {
+		if (pageName === 'reflections') return <Home />;
+		if (pageName === 'focals') return <Focals />;
+		if (pageName === 'chat') return <Chat />;
+		if (pageName === 'qve') return <QvE editorAPI={ wordEditorAPI } />;
+		console.error('Invalid page name', pageName);
+	}
+
+	return <Layout>{ getComponent(page) }</Layout>;
 }
