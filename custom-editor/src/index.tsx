459751dd49fa1dad6dcb55e1cd0f7c44c943d62d
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-10-15 09:37:35 -0400

    Custom editor has been merged into main frontend code as add-in/src/editor.html.
    Ray already merged useful things from this code into that place, so this code is no longer needed.

--- custom-editor/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-
-import App from './pages/app';
-import './globals.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-
-root.render(<App />);
--- custom-editor/src/settings.ts
+++
@@ -1,3 +0,0 @@
-export const DEBUG = true; // TODO: Change this to an ENV var
-
-export const SERVER_URL = DEBUG ? 'http://localhost:8000' : '';
