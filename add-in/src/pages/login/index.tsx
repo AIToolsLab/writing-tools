312349a42770b5ab3684444e3c99204d98db0ef5
Cobblestone5517 <kjy11gold@gmail.com>
2024-11-12 16:57:54 -0500

    Not needed anymore, remove it in the future

--- add-in/src/pages/login/index.tsx
+++ add-in/src/pages/login/index.tsx
@@ -5,8 +5,8 @@ import { UserContext } from '@/contexts/userContext';
 import { PageContext } from '@/contexts/pageContext';  // pasted from the branch auth0
 
 // TODO FIXME don't copy-paste this from papup.tsx - pasted from the branch auth0
-const auth0Subdomain = 'dev-62nhczyl7e1oaj8a.us.auth0.com';
-const auth0ClientId = 'W6MVTHKKbejEq7tCcT2oLt8gStOeHxT7';
+const auth0Subdomain: string = 'dev-rbroo1fvav24wamu.us.auth0.com';
+const auth0ClientId: string = 'YZhokQZRgE2YUqU5Is9LcaMiCzujoaVr';
 
 import classes from './styles.module.css';
 
@@ -15,6 +15,59 @@ export default function Login() {
 	const { changePage } = useContext(PageContext);
 
 	let dialog: Office.Dialog;
+
+	// async function processMessage(
+	// 	args:
+	// 		| { message: string; origin: string | undefined }
+	// 		| { error: number }
+	// ) {
+	// 	if ('error' in args) {
+	// 		// eslint-disable-next-line no-console
+	// 		console.error('Error:', args.error);
+	// 		return;
+	// 	}
+	// 	// eslint-disable-next-line prefer-const
+	// 	let messageFromDialog = JSON.parse(args.message);
+	// 	dialog.close();
+
+	// 	if (messageFromDialog.status === 'success') {
+	// 		// The dialog reported a successful login.
+	// 		// eslint-disable-next-line prefer-const
+	// 		let token = messageFromDialog.auth0Token;
+	// 		// eslint-disable-next-line no-console
+	// 		console.log('Login successful.', token);
+
+	// 		window.dispatchEvent(
+	// 			new MessageEvent('message', {
+	// 				data: {
+	// 					type: 'auth0Token',
+	// 					response: token
+	// 				}
+	// 			})
+	// 		)
+
+	// 		// Get the userinfo from auth0
+	// 		// eslint-disable-next-line prefer-const
+	// 		let userinfoResponse = await fetch(
+	// 			`https://${auth0Subdomain}/userinfo?access_token=${token}`
+	// 		);
+	// 		// eslint-disable-next-line prefer-const
+	// 		let userinfo = await userinfoResponse.json();
+	// 		// eslint-disable-next-line no-console
+	// 		console.log('Userinfo:', userinfo);
+
+	// 		// Make a test authenticated request to the server
+	// 		// eslint-disable-next-line prefer-const
+	// 		let testResponse = await fetch('/api/private', {
+	// 			headers: {
+	// 				Authorization: `Bearer ${token}`
+	// 			}
+	// 		});
+	// 	} else {
+	// 		// eslint-disable-next-line no-console
+	// 		console.error('Login failed.', messageFromDialog);
+	// 	}
+	// }
 
 	async function processMessage(
 		args:
@@ -36,24 +89,22 @@ export default function Login() {
 			let token = messageFromDialog.auth0Token;
 			// eslint-disable-next-line no-console
 			console.log('Login successful.', token);
-			// Get the userinfo from auth0
-			// eslint-disable-next-line prefer-const
-			let userinfoResponse = await fetch(
-				`https://${auth0Subdomain}/userinfo?access_token=${token}`
-			);
-			// eslint-disable-next-line prefer-const
-			let userinfo = await userinfoResponse.json();
-			// eslint-disable-next-line no-console
-			console.log('Userinfo:', userinfo);
 
-			// Make a test authenticated request to the server
-			// eslint-disable-next-line prefer-const
-			let testResponse = await fetch('/api/private', {
-				headers: {
-					Authorization: `Bearer ${token}`
-				}
-			});
-		} else {
+			// Mock the window message event that auth0-spa-js expects
+			window.dispatchEvent(
+				// look up what the messagevent is in the Auth0 app
+				new MessageEvent('message', {
+					data: {
+						origin: `https://${auth0Subdomain}`,
+						data: {
+							type: 'authorization_response',
+							response: token // or something like that, mocking whatever the popup actually sends
+						}
+					}
+				})
+			);
+		}
+		else {
 			// eslint-disable-next-line no-console
 			console.error('Login failed.', messageFromDialog);
 		}
@@ -65,31 +116,7 @@ export default function Login() {
 
 	const [userId, updateId] = useState('');
 
-	// return (
-	// 	<div className={ classes.container }>
-	// 		<input
-	// 			value={ userId }
-	// 			placeholder="Username"
-	// 			onChange={ e => updateId(e.target.value) }
-	// 			onKeyDown={ e => {
-	// 				if (e.key === 'Enter' && userId.length > 0)
-	// 					updateUsername(userId);
-	// 			} }
-	// 		/>
 
-	// 		<button
-	// 			disabled={ userId.length === 0 }
-	// 			onClick={ () => updateUsername(userId) }
-	// 		>
-	// 			Login
-	// 		</button>
-
-	// 		<div className={ classes.widthAlert }>
-	// 			{ width < 400 &&
-	// 				'For best experience please expand the sidebar by dragging the splitter.' }
-	// 		</div>
-	// 	</div>
-	// );
 	return (
 		<div className={ classes.container  }>
 			<button
@@ -105,6 +132,7 @@ export default function Login() {
 						'/popup.html';
 
 					// height and width are percentages of the size of the screen.
+					// How MS use it: https://github.com/OfficeDev/Office-Add-in-samples/blob/main/Samples/auth/Office-Add-in-Microsoft-Graph-React/utilities/office-apis-helpers.ts#L38
 					Office.context.ui.displayDialogAsync(
 						fullUrl,
 						{ height: 45, width: 55 },
