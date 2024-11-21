2795bf70ce859ae70281c82e0940d441ab467bde
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-11-21 09:48:53 -0500

    DEBUGGING: conditionally import server

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,8 +1,6 @@
 import azure.functions as func
 import logging
 
-import server
-
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="HttpExample")
@@ -25,3 +23,27 @@ def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.route(route="HttpExample2")
+def HttpExample2(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('About to import server!')
+    import server
+    logging.info('Imported server!')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. You win.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+    
