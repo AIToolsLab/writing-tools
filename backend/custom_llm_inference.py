8218a79f9b6a63b06fb7362b5578e1461099b764
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-12-02 15:27:33 -0500

    refactor custom LLM code, part 1

---
+++ backend/custom_llm_inference.py
@@ -0,0 +1,100 @@
+import torch
+
+def get_highlights_inner(model, tokenizer, doc, prompt, updated_doc, k):
+    messages = [
+        {
+            "role": "user",
+            "content": f"{prompt}\n\n{doc}",
+        },
+    ]
+    tokenized_chat = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")[0]
+    assert len(tokenized_chat.shape) == 1
+
+    if updated_doc is None or len(updated_doc.strip()) == 0:
+        updated_doc = doc
+    updated_doc_ids = tokenizer(updated_doc, return_tensors='pt')['input_ids'][0]
+
+    joined_ids = torch.cat([tokenized_chat, updated_doc_ids[1:]])
+    # Call the model
+    with torch.no_grad():
+        logits = model(joined_ids[None].to(model.device)).logits[0].cpu()
+    
+    highlights = []
+    length_so_far = 0
+    for idx in range(len(tokenized_chat), len(joined_ids)):
+        probs = logits[idx - 1].softmax(dim=-1)
+        token_id = joined_ids[idx]
+        token = tokenizer.decode(token_id)
+        token_loss = -probs[token_id].log().item()
+        topk_tokens = probs.topk(k).indices.cpu().numpy().tolist()
+        topk_tokens_decoded = tokenizer.batch_decode(topk_tokens, skip_special_tokens=True)
+        highlights.append(dict(
+            start=length_so_far,
+            end=length_so_far + len(token),
+            token=token,
+            token_loss=token_loss,
+            most_likely_token=topk_tokens_decoded[0],
+            topk_tokens=topk_tokens_decoded,
+        ))
+        length_so_far += len(token)
+    return highlights
+
+
+def get_next_token_predictions_inner(
+        model, tokenizer, original_doc, prompt, doc_in_progress, k):
+    
+    messages = [
+        {
+            "role": "user",
+            "content": f"{prompt}\n\n{original_doc}",
+        },
+    ]
+    tokenized_chat = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")[0]
+
+    doc_in_progress_ids = tokenizer(doc_in_progress, return_tensors='pt')['input_ids'][0]
+
+    # strip the first token, the "beginning of document" token
+    doc_in_progress_ids = doc_in_progress_ids[1:]
+
+    joined_ids = torch.cat([tokenized_chat, doc_in_progress_ids])
+    hypotheses = joined_ids[None].to(model.device)
+
+    # For each of the k next tokens, generate most-likely next tokens and append back on until we
+    # reach a token with a space
+
+    with torch.no_grad():
+        model_outs = model(hypotheses, output_hidden_states=True)
+
+    next_token_logits = model_outs.logits[0, -1]
+    branch_tokens = next_token_logits.topk(k).indices
+
+    # Now call the model again, passing the kv cache, so we can continue generating.
+    # Each of the k next tokens will be considered as one sequence in a "batch".
+    next_tokens_as_batch = branch_tokens.unsqueeze(1)
+    assert next_tokens_as_batch.shape == (k, 1)
+
+    # We need to duplicate the kv cache for each of the k next tokens
+    kv_cache = [
+        (key.repeat_interleave(k, dim=0), value.repeat_interleave(k, dim=0))
+        for key, value in model_outs.past_key_values
+    ]
+
+    with torch.no_grad():
+        model_outs = model(next_tokens_as_batch, past_key_values=kv_cache, output_hidden_states=True)
+    
+    # Grab the single most likely token from each of the k sequences
+    next_token_logits = model_outs.logits[:, -1]
+    vocab_size = len(tokenizer)
+    assert next_token_logits.shape == (k, vocab_size), f"{next_token_logits.shape=}, {k=}, {vocab_size=}"
+    most_likely_token_ids = next_token_logits.argmax(dim=-1)
+
+    # Stick them at the end of the branch tokens.
+    assert most_likely_token_ids.shape == (k,)
+    lookahead_sequences = torch.cat([
+        branch_tokens.unsqueeze(1),
+        most_likely_token_ids.unsqueeze(1)
+    ], dim=1)
+    assert lookahead_sequences.shape == (k, 2)
+
+    decoded_next_tokens = tokenizer.batch_decode(lookahead_sequences, skip_special_tokens=True)
+    return decoded_next_tokens, next_token_logits
