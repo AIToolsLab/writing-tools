f795dd778056098ac2080afbd9d3040f7f0ec80c
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-12-21 13:25:56 -0800

    minor fixes

--- frontend/src/pages/app/index.tsx
+++ frontend/src/pages/app/index.tsx
@@ -1,13 +1,12 @@
 import { useContext, useRef, useEffect } from 'react';
 import { pingServer } from '@/api';
 
-import { PageContext } from '@/contexts/pageContext';
 import { CgFacebook, CgGoogle, CgMicrosoft } from 'react-icons/cg';
 import { useWindowSize } from '@react-hook/window-size/throttled';
 
 import { useAuth0, Auth0Provider } from '@auth0/auth0-react';
-// eslint-disable-next-line no-duplicate-imports
-import PageContextWrapper, { PageName } from '@/contexts/pageContext';
+
+import PageContextWrapper, { PageName, PageContext } from '@/contexts/pageContext';
 import UserContextWrapper from '@/contexts/userContext';
 import ChatContextWrapper from '@/contexts/chatContext';
 
