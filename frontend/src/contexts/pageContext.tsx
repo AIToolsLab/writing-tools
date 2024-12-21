e342f079b533243164231e63a4c195d44b1df3cc
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-12-21 13:15:43 -0800

    address typechecker concerns

--- frontend/src/contexts/pageContext.tsx
+++ frontend/src/contexts/pageContext.tsx
@@ -1,14 +1,24 @@
 import { PropsWithChildren, createContext, useState } from 'react';
+export enum PageName {
+	Revise = 'revise',
+	SearchBar = 'searchbar',
+	Chat = 'chat',
+	Draft = 'draft'
+  }
 
-export const PageContext = createContext({
-	page: 'draft',
-	changePage: (_page: string) => {}
+
+export const PageContext = createContext<{
+  page: PageName;
+  changePage: (page: PageName) => void;
+}>({
+  page: PageName.Draft,
+  changePage: () => {},
 });
 
 export default function PageContextWrapper({
 	children
 }: PropsWithChildren<any>) {
-	const [page, updatePage] = useState('draft');
+	const [page, updatePage] = useState(PageName.Draft);
 
 	return (
 		<PageContext.Provider value={ { page, changePage: updatePage } }>
