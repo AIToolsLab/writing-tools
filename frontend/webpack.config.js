afd510bc372be85a2527a88e0de3a45151b8e292
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2025-03-25 14:57:23 -0400

    use localhost backend server for dev

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,6 +11,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const urlDev = 'https://localhost:3000';
 const urlProd = 'https://app.thoughtful-ai.com';
 
+const backendDev = 'http://0.0.0.0:8000/';
+const backendProd = 'https://textfocals.azurewebsites.net/';
+
 async function getHttpsOptions() {
 	const httpsOptions = await devCerts.getHttpsServerOptions();
 	return {
@@ -165,7 +168,7 @@ module.exports = async (env, options) => {
 			proxy: [
 				{
 					context: ['/api'],
-					target: 'https://textfocals.azurewebsites.net/',
+					target: dev ? backendDev : backendProd,
 					changeOrigin: true
 				}
 			],
