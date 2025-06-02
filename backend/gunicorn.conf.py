696b9ae0a6bf858978324e3b047ea5c96a5012dc
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2025-06-02 10:35:42 -0400

    use stdout for logs
    So we can use `docker-compose logs --follow`

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -3,8 +3,8 @@ worker_class = "uvicorn.workers.UvicornWorker"
 wsgi_app = "server:app"
 # We haven't tested with multiple workers (and probably not necessary anyway).
 workers = 1
-errorlog = "error.log"
-accesslog = "access.log"
+errorlog = "-"
+accesslog = "-"
 capture_output = False
 proc_name = "writing-tools"
 pidfile = "gunicorn.pid"
