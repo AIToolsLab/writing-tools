1b5dd1bcff9b353df38354fa8bfbe5280ef7238d
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2025-06-06 11:57:22 -0400

    Oops, forgot to include the changes to the draft page itself.

--- frontend/src/pages/draft/index.tsx
+++ frontend/src/pages/draft/index.tsx
@@ -8,12 +8,12 @@ import { Fragment, useContext, useState } from 'react';
 import {
 	AiOutlineAlignLeft,
 	AiOutlineBank,
+	AiOutlineClose,
 	AiOutlineHighlight,
 	AiOutlineQuestion,
 } from 'react-icons/ai';
 import { Remark } from 'react-remark';
 import { iconFunc } from './iconFunc';
-import SavedGenerations from './savedGenerations';
 import classes from './styles.module.css';
 
 const visibleNameForMode = {
@@ -34,6 +34,83 @@ const visibleIconForMode = {
 function GenerationResult({ generation }: { generation: GenerationResult }) {
 	return <Remark>{ generation.result }</Remark>;
 }
+
+function SavedGenerations({ 
+    savedItems, 
+    deleteSavedItem,
+}: { 
+    savedItems: SavedItem[],
+    deleteSavedItem: (dateSaved: Date) => void,
+}) {
+
+
+    return (
+        <div className={ classes.historyContainer }>
+
+
+            <div className={ classes.historyItemContainer }>
+
+                { /* can we use || ? */ }
+
+                { savedItems.length === 0 ? (
+                    <div className={ classes.historyEmptyWrapper }>
+                        <div className={ classes.historyText }>
+                            No saved generations... 
+                        </div>
+                    </div>
+                ) : (
+                    savedItems.map((savedItem, index) => (
+                        <div
+                            key={ index }
+                            className={ classes.historyItem }
+                        >
+                            <div className={ classes.historyText }>
+                                <p className={ classes.historyDoc }  
+                                onClick={ () => { 
+                                    // Show the whole document text
+                                } }     >
+                                    ...
+                                    { savedItem.document.substring(savedItem.document.length - 100    ) }
+                                </p>
+
+                                <GenerationResult generation={ savedItem.generation } />
+                            </div>
+                            <div
+                                className={ classes.savedIconsContainer }
+                            >
+                                <div
+                                    className={
+                                        classes.historyCloseButtonWrapper
+                                    }
+                                    onClick={ () =>
+                                        deleteSavedItem(
+                                            savedItem.dateSaved
+                                        )
+                                    }
+                                >
+                                    <AiOutlineClose
+                                        className={
+                                            classes.historyCloseButton
+                                        }
+                                    />
+                                </div>
+                                <div
+                                    className={
+                                        classes.genTypeIconWrapper
+                                    }
+                                >
+                                    { iconFunc(savedItem.generation.generation_type) }
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                ) }
+            </div>
+        </div>
+
+    );
+}
+
 
 export default function Draft() {
 	const editorAPI = useContext(EditorContext);
