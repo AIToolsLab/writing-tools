deb1fac07c7a0edaa0075ed6b44c8902bce5bccd
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2025-06-06 13:55:39 -0400

    run prettier to format new code

--- frontend/src/pages/study/index.tsx
+++ frontend/src/pages/study/index.tsx
@@ -5,102 +5,90 @@ import { usernameAtom } from '@/contexts/userContext';
 import { useDocContext } from '@/utilities';
 import { getBefore } from '@/utilities/selectionUtil';
 import { Fragment, useContext, useState } from 'react';
-import {
-	AiOutlineClose,
-} from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
 import { Remark } from 'react-remark';
 import { iconFunc } from './iconFunc';
 import classes from './styles.module.css';
 import { useAtomValue } from 'jotai';
 
 const visibleNameForMode = {
-	'Completion': 'Suggestions',
-	'Question': 'Questions',
-	'Keywords': 'Keywords',
-	'RMove': 'Rhetorical Move'
+	Completion: 'Suggestions',
+	Question: 'Questions',
+	Keywords: 'Keywords',
+	RMove: 'Rhetorical Move'
 };
 
-
 function GenerationResult({ generation }: { generation: GenerationResult }) {
-	return <Remark>{ generation.result }</Remark>;
+	return <Remark>{generation.result}</Remark>;
 }
 
-function SavedGenerations({ 
-    savedItems, 
-    deleteSavedItem,
-}: { 
-    savedItems: SavedItem[],
-    deleteSavedItem: (dateSaved: Date) => void,
+function SavedGenerations({
+	savedItems,
+	deleteSavedItem
+}: {
+	savedItems: SavedItem[];
+	deleteSavedItem: (dateSaved: Date) => void;
 }) {
+	return (
+		<div className={classes.historyContainer}>
+			<div className={classes.historyItemContainer}>
+				{/* can we use || ? */}
 
+				{savedItems.length === 0 ? (
+					<div className={classes.historyEmptyWrapper}>
+						<div className={classes.historyText}>
+							No saved generations...
+						</div>
+					</div>
+				) : (
+					savedItems.map((savedItem, index) => (
+						<div
+							key={index}
+							className={classes.historyItem}
+						>
+							<div className={classes.historyText}>
+								<p
+									className={classes.historyDoc}
+									onClick={() => {
+										// Show the whole document text
+									}}
+								>
+									...
+									{savedItem.document.substring(
+										savedItem.document.length - 100
+									)}
+								</p>
 
-    return (
-        <div className={ classes.historyContainer }>
-
-
-            <div className={ classes.historyItemContainer }>
-
-                { /* can we use || ? */ }
-
-                { savedItems.length === 0 ? (
-                    <div className={ classes.historyEmptyWrapper }>
-                        <div className={ classes.historyText }>
-                            No saved generations... 
-                        </div>
-                    </div>
-                ) : (
-                    savedItems.map((savedItem, index) => (
-                        <div
-                            key={ index }
-                            className={ classes.historyItem }
-                        >
-                            <div className={ classes.historyText }>
-                                <p className={ classes.historyDoc }  
-                                onClick={ () => { 
-                                    // Show the whole document text
-                                } }     >
-                                    ...
-                                    { savedItem.document.substring(savedItem.document.length - 100    ) }
-                                </p>
-
-                                <GenerationResult generation={ savedItem.generation } />
-                            </div>
-                            <div
-                                className={ classes.savedIconsContainer }
-                            >
-                                <div
-                                    className={
-                                        classes.historyCloseButtonWrapper
-                                    }
-                                    onClick={ () =>
-                                        deleteSavedItem(
-                                            savedItem.dateSaved
-                                        )
-                                    }
-                                >
-                                    <AiOutlineClose
-                                        className={
-                                            classes.historyCloseButton
-                                        }
-                                    />
-                                </div>
-                                <div
-                                    className={
-                                        classes.genTypeIconWrapper
-                                    }
-                                >
-                                    { iconFunc(savedItem.generation.generation_type) }
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                ) }
-            </div>
-        </div>
-
-    );
+								<GenerationResult
+									generation={savedItem.generation}
+								/>
+							</div>
+							<div className={classes.savedIconsContainer}>
+								<div
+									className={
+										classes.historyCloseButtonWrapper
+									}
+									onClick={() =>
+										deleteSavedItem(savedItem.dateSaved)
+									}
+								>
+									<AiOutlineClose
+										className={classes.historyCloseButton}
+									/>
+								</div>
+								<div className={classes.genTypeIconWrapper}>
+									{iconFunc(
+										savedItem.generation.generation_type
+									)}
+								</div>
+							</div>
+						</div>
+					))
+				)}
+			</div>
+		</div>
+	);
 }
-
 
 export default function Draft() {
 	const editorAPI = useContext(EditorContext);
@@ -122,15 +110,16 @@ export default function Draft() {
 	// Update Error Message
 	const [errorMsg, updateErrorMsg] = useState('');
 
-
-
 	// Save the generation
 	function save(generation: GenerationResult, document: string) {
-		const newSaved = [...savedItems, {
-			document: document,
-			generation: generation,
-			dateSaved: new Date()
-		}];
+		const newSaved = [
+			...savedItems,
+			{
+				document: document,
+				generation: generation,
+				dateSaved: new Date()
+			}
+		];
 
 		updateSavedItems(newSaved);
 	}
@@ -162,7 +151,6 @@ export default function Draft() {
 
 	const beforeContext = getBefore(docContext);
 
-
 	// Get a generation from the backend
 	async function getGeneration(
 		username: string,
@@ -186,7 +174,7 @@ export default function Draft() {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
-					'Authorization': `Bearer ${token}`
+					Authorization: `Bearer ${token}`
 				},
 				body: JSON.stringify({
 					username: username,
@@ -199,12 +187,11 @@ export default function Draft() {
 				throw new Error(`HTTP error! status: ${response.status}`);
 			}
 			updateErrorMsg('');
-			const generated = await response.json() as GenerationResult;
+			const generated = (await response.json()) as GenerationResult;
 			updateGeneration(generated);
 			updateGenCtxText(contextText);
 			save(generated, contextText);
-		}
-		catch (err: any) {
+		} catch (err: any) {
 			setIsLoading(false);
 			let errMsg = '';
 			if (err.name === 'AbortError')
@@ -225,22 +212,23 @@ export default function Draft() {
 		setIsLoading(false);
 	}
 
-
 	// Temporarily select the text from the start to the cursor
 	async function _selectToCursor(duration: number = 1000): Promise<void> {
-    try {
+		try {
 			await Word.run(async (context: Word.RequestContext) => {
 				// TODO: Instead, use the "wordSelection" from the wordEditorAPI.ts
 				const body = context.document.body;
 				const wordSelection = context.document
-        .getSelection()
-        .getTextRanges([' '], false);
+					.getSelection()
+					.getTextRanges([' '], false);
 
 				context.load(wordSelection, 'items');
 				await context.sync();
 
 				// Get the range from start to the end of current word
-				const rangeToCursor = wordSelection.items[wordSelection.items.length-1].expandTo(body.getRange('Start'));
+				const rangeToCursor = wordSelection.items[
+					wordSelection.items.length - 1
+				].expandTo(body.getRange('Start'));
 
 				// Select the range
 				rangeToCursor.select();
@@ -249,33 +237,32 @@ export default function Draft() {
 				// Unselect after specified duration
 				setTimeout(async () => {
 					await Word.run(async (context: Word.RequestContext) => {
-						const rangeToCursor = wordSelection.items[wordSelection.items.length-1].expandTo(body.getRange('Start'));
+						const rangeToCursor = wordSelection.items[
+							wordSelection.items.length - 1
+						].expandTo(body.getRange('Start'));
 						rangeToCursor.select();
 						await context.sync();
 					});
 				}, duration);
 			});
-    }
-		catch (error) {
+		} catch (error) {
 			// eslint-disable-next-line no-console
 			console.error('Error highlighting text:', error);
-    }
+		}
 	}
 
 	if (authErrorType !== null) {
-        return (
-            <div>
-							Please reauthorize.
-						</div>
-        );
-    }
+		return <div>Please reauthorize.</div>;
+	}
 
 	let results = null;
 
 	if (errorMsg !== '')
 		results = (
-			<div className= "mr-[16px] ml-[16px] p-[16px] duration-150">
-				<div className="text-base text-red-500 text-center">{ errorMsg }</div>
+			<div className="mr-[16px] ml-[16px] p-[16px] duration-150">
+				<div className="text-base text-red-500 text-center">
+					{errorMsg}
+				</div>
 			</div>
 		);
 	else if (generation === null)
@@ -299,24 +286,18 @@ export default function Draft() {
 		results = (
 			<div className="mt-1 mr-2 ml-2 p-4 border border-[#c8c8c8] rounded-[16px] transition duration-150 flex">
 				<div>
-					<div
-						className= "text-[0.8rem] text-[#aaaaaa] pb-1 cursor-pointer hover:text-black"
-					>
-						{ genCtxText.length > 100 ? '...' : '' }
-						{ genCtxText.substring(genCtxText.length - 100) }
+					<div className="text-[0.8rem] text-[#aaaaaa] pb-1 cursor-pointer hover:text-black">
+						{genCtxText.length > 100 ? '...' : ''}
+						{genCtxText.substring(genCtxText.length - 100)}
 					</div>
 
 					<div className="text-base whitespace-pre-wrap transition duration-150 animate-fade-in">
-						<GenerationResult generation={ generation } />
+						<GenerationResult generation={generation} />
 					</div>
 				</div>
-				<div className={ classes.genIconsContainer }>
-					<div
-						className={
-								classes.genTypeIconWrapper
-						}
-					>
-						{ iconFunc(generation.generation_type) }
+				<div className={classes.genIconsContainer}>
+					<div className={classes.genTypeIconWrapper}>
+						{iconFunc(generation.generation_type)}
 					</div>
 				</div>
 			</div>
@@ -324,61 +305,71 @@ export default function Draft() {
 
 	if (isLoading && !generation)
 		results = (
-			<div className={ classes.spinnerWrapper }>
-				<div className={ classes.loader }></div>
+			<div className={classes.spinnerWrapper}>
+				<div className={classes.loader}></div>
 			</div>
 		);
 
 	return (
 		<div className=" flex flex-col gap-2 relative p-2">
-
-			{ /* Document Context Text Container */ }
-			<div className= "text-sm p-[8px] m-[8px] shadow-[0_6px_10px_-1px_rgba(147,123,109,0.1)]">
+			{/* Document Context Text Container */}
+			<div className="text-sm p-[8px] m-[8px] shadow-[0_6px_10px_-1px_rgba(147,123,109,0.1)]">
 				<h4>Suggestions will be generated using:</h4>
 				<p>
-					{ beforeContext.length > 100 ? '...' : '' }
-					{ beforeContext.substring(beforeContext.length - 100) }
-					{ /* { JSON.stringify(docContext) } */ }
+					{beforeContext.length > 100 ? '...' : ''}
+					{beforeContext.substring(beforeContext.length - 100)}
+					{/* { JSON.stringify(docContext) } */}
 				</p>
 			</div>
 
 			<div>
-				{ /* Generation Option Buttons */ }
-				<div
-					className={ classes.optionsContainer }
-				>
-					{ ['Completion', 'Question', 'Keywords', 'RMove'].map(mode => {
-						return (
-							<Fragment key={ mode }>
-							<button
-								className={ classes.optionsButton }
-								disabled={ docContext.beforeCursor === '' || isLoading }
-								title={ visibleNameForMode[mode as keyof typeof visibleNameForMode] }
-								aria-label={ visibleNameForMode[mode as keyof typeof visibleNameForMode] }
-								onClick={ async () => {
-									log({
-										username: username,
-										interaction: `${mode}_Frontend`,
-										prompt: beforeContext
-									});
-									if (beforeContext === '') return;
+				{/* Generation Option Buttons */}
+				<div className={classes.optionsContainer}>
+					{['Completion', 'Question', 'Keywords', 'RMove'].map(
+						mode => {
+							return (
+								<Fragment key={mode}>
+									<button
+										className={classes.optionsButton}
+										disabled={
+											docContext.beforeCursor === '' ||
+											isLoading
+										}
+										title={
+											visibleNameForMode[
+												mode as keyof typeof visibleNameForMode
+											]
+										}
+										aria-label={
+											visibleNameForMode[
+												mode as keyof typeof visibleNameForMode
+											]
+										}
+										onClick={async () => {
+											log({
+												username: username,
+												interaction: `${mode}_Frontend`,
+												prompt: beforeContext
+											});
+											if (beforeContext === '') return;
 
-									getGeneration(
-										username,
-										`${mode}_Backend`,
-										beforeContext
-									);
-								} }
-							>
-							   { iconFunc(mode) }
-							</button>
-							</Fragment>
-						);
-					}) }
-					</div>
+											getGeneration(
+												username,
+												`${mode}_Backend`,
+												beforeContext
+											);
+										}}
+									>
+										{iconFunc(mode)}
+									</button>
+								</Fragment>
+							);
+						}
+					)}
+				</div>
 
-				<div className={ classes.noteTextWrapper }>
-					<div className={ classes.noteText }>
+				<div className={classes.noteTextWrapper}>
+					<div className={classes.noteText}>
 						Please note that the quality of AI-generated text may
 						vary
 					</div>
@@ -386,13 +377,13 @@ export default function Draft() {
 			</div>
 
 			<div>
-				{ /* Result of the generation */ }
-				<div className={ classes.reflectionContainer }>{ results }</div>
+				{/* Result of the generation */}
+				<div className={classes.reflectionContainer}>{results}</div>
 
-				{ /* Saved generations */ }
+				{/* Saved generations */}
 				<SavedGenerations
-					savedItems={ savedItems }
-					deleteSavedItem={ deleteSavedItem }
+					savedItems={savedItems}
+					deleteSavedItem={deleteSavedItem}
 				/>
 			</div>
 		</div>
