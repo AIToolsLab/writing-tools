95a0a04cd3ce15d7d52efbfa5465dfd5388011a9
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2025-06-06 12:32:31 -0400

    Finish username atom changes (forgot to do this in last commit)

--- frontend/src/pages/searchbar/index.tsx
+++ frontend/src/pages/searchbar/index.tsx
@@ -4,7 +4,7 @@ import { ReflectionCards } from '@/components/reflectionCard';
 import { SearchBox } from '@/components/searchBox';
 import { RhetoricalContextBox } from '@/components/rhetoricalContextBox';
 
-import { UserContext } from '@/contexts/userContext';
+import { usernameAtom } from '@/contexts/userContext';
 
 import { getParagraphText } from '@/utilities';
 import { getReflection } from '@/api';
@@ -12,9 +12,10 @@ import { getReflection } from '@/api';
 import { useAuth0 } from '@auth0/auth0-react';
 
 import classes from './styles.module.css';
+import { useAtomValue } from 'jotai';
 
 export default function SearchBar() {
-	const { username } = useContext(UserContext);
+	const username = useAtomValue(usernameAtom);
 
 	const { getAccessTokenSilently } = useAuth0();
 
