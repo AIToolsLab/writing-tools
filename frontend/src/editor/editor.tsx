8043f7064bf07bb1f12fe33a17da84e0db4ff14f
Jooha Yoo <jy49@calvin.edu>
2025-06-25 14:42:45 -0400

    getting contentEditable component back to using editor class so we don't get red borders

--- frontend/src/editor/editor.tsx
+++ frontend/src/editor/editor.tsx
@@ -174,7 +174,7 @@ function LexicalEditor({
 					<div className="whitespace-pre-line border-b-2">{taskPrompt}</div>
 					<RichTextPlugin
 						contentEditable={
-							<ContentEditable className="" />
+							<ContentEditable className={classes.editor} />
 						}
 						placeholder={ <div className={ classes.placeholder } /> }
 						ErrorBoundary={ LexicalErrorBoundary }
