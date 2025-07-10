2eaa8c5255a16c4197801511e2632378aeb5bdca
Jooha Yoo <jy49@calvin.edu>
2025-07-10 15:30:38 -0400

    assigning generic labels (A,B,C) by task order

--- frontend/src/editor/index.tsx
+++ frontend/src/editor/index.tsx
@@ -254,6 +254,15 @@ However, CRISPR also raises important ethical questions, particularly regarding 
 		return result;
 	}
 
+		function mapInputToLabels(input: string) {
+			const result: Record<string, { condition: string }> = {};
+			const taskLabel = ['Type A', 'Type B', 'Type C'];
+			input.split('').forEach((_, idx) => {
+				result[(idx + 1).toString()] = { condition: taskLabel[idx] };
+			});
+			return result;
+	}
+
 function Router({
 	page
 }: {
@@ -297,6 +306,7 @@ function Router({
 		}
 
 		const conditionConfigs = mapInputToDict(conditionOrder);
+		const labelConfigs = mapInputToLabels(conditionOrder);
 
 		const studyPageIndex = studyPageNames.indexOf(page);
 		if (studyPageIndex === -1) {
@@ -307,8 +317,8 @@ function Router({
 
 		if (page === 'study-consentForm') {
 			const nextUrlParams = new URLSearchParams(window.location.search);
-      nextUrlParams.set('page', nextPage);
-      const redirectURL = encodeURIComponent(window.location.origin + `/editor.html?${nextUrlParams.toString()}`);
+			nextUrlParams.set('page', nextPage);
+			const redirectURL = encodeURIComponent(window.location.origin + `/editor.html?${nextUrlParams.toString()}`);
 			const consentFormURL = SURVEY_URLS.consentForm;
 
 			return <div className={classes.studyIntroContainer}>
@@ -385,10 +395,11 @@ function Router({
 			}
 
 			const taskCondition = conditionConfig.condition;
+			const taskLabel = conditionConfig.condition;
 
 			return (
 				<div className={classes.studyIntroContainer}>
-					<p> Now we'll start the task {taskNumber} out of 3. <br/> In this task, you'll using writing assistance system {taskCondition} </p>
+					<p> Now we'll start the task {taskNumber} out of 3. <br/> In this task, you'll using writing assistance system {taskLabel} </p>
 					<button
 						onClick={() => {
 							log({
@@ -458,16 +469,19 @@ function Router({
 			const redirectURL = encodeURIComponent(window.location.origin + `/editor.html?${nextUrlParams.toString()}`);
 			const taskNumber = page.replace('study-postTask', '');
 			const conditionConfig = conditionConfigs[taskNumber as keyof typeof conditionConfigs];
+			const labelConfig = labelConfigs[taskNumber as keyof typeof labelConfigs];
 
 			if (!conditionConfig) {
 				return <div>Invalid task number</div>;
 			}
 
 			const taskCondition = conditionConfig.condition;
+			const taskLabel = labelConfig.condition;
+
 			const postTaskSurveyURL = SURVEY_URLS.postTask;
 
 			return <div className={classes.studyIntroContainer}>
-				<p> Thank you for completing Task {taskNumber}. Please take a moment to complete a brief survey.</p>
+				<p> Thank you for completing Task {taskNumber} using Suggestion {taskLabel}. <br/> Please take a moment to complete a brief survey.</p>
 				<a
 					onClick={() => {
 						log ({
