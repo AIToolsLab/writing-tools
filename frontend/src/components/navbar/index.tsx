b7edb3de09e3cbdf3bc364fa131120f2ab198b9d
Jooha Yoo <jy49@calvin.edu>
2026-03-24 23:36:05 -0400

    formatting

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -23,21 +23,9 @@ type Page = {
 };
 
 const pageNames: Page[] = [
-	{
-		name: PageName.Draft,
-		title: 'Draft',
-		hint: 'Generate suggestions',
-	},
-	{
-		name: PageName.Revise,
-		title: 'Revise',
-		hint: 'Improve your text',
-	},
-	{
-		name: PageName.Chat,
-		title: 'Chat',
-		hint: 'Ask about your doc',
-	},
+	{ name: PageName.Draft, title: 'Draft', hint: 'Generate suggestions' },
+	{ name: PageName.Revise, title: 'Revise', hint: 'Improve your text' },
+	{ name: PageName.Chat, title: 'Chat', hint: 'Ask about your doc' },
 ];
 
 export default function Navbar() {
